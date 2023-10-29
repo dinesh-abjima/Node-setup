5ceee4c6cde7216153c15d5abdd6f091f244a373
salik-abjima <85283147+salik-abjima@users.noreply.github.com>
2023-10-29 22:36:15 +0530

    added logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AwsModule } from './config/aws/aws.module';
+import { LoggerMiddleware } from './utils/logger.middleware';
 
 @Module({
   imports: [DatabaseModule, AwsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
